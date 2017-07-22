13f4d6c55cd2d1492ddf74ebf5589dc2df18cb16
Daniel McClure <contact@danielmcclure.com>
2017-07-22 11:06:04 -0400

    Convert to GBP & Add Search

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# wp-job-manager-salary-field
-Adds salary field to the frontend and backend for WP Job Manager.
+# WP Job Manager - GBP Salary Add-On
+
+Adds GBP Salary field to the frontend, backend & search for WP Job Manager.
+
+Code based upon: [Tutorial: Adding a salary field for jobs](https://wpjobmanager.com/document/tutorial-adding-a-salary-field-for-jobs/)
+
+> This plugin is an extension for [WP Job Manager](https://wpjobmanager.com/)
--- wp-job-manager-salary-field.php
+++ wp-job-manager-salary-field.php
@@ -1,17 +1,17 @@
 <?php
 /*
-Plugin Name: WP Job Manager Salary Field
+Plugin Name: WP Job Manager - GBP Salary Add-On
 Plugin URI: https://wpjobmanager.com/
-Description: Adds salary field to the frontend and backend for WP Job Manager.
+Description: Adds GBP Salary field to the frontend, backend & search for WP Job Manager.
 Version: 1.0
-Author: Mike Jolley
-Author URI: http://mikejolley.com
+Author: danielmcclure
+Author https://github.com/danielmcclure/wp-job-manager-salary-field
 Requires at least: 4.1
-Tested up to: 4.3
+Tested up to: 4.8
 Text Domain: wp-job-manager
 Domain Path: /languages
 
-	Copyright: 2015 Mike Jolley
+	Copyright: 2015 Mike Jolley & 2017 Daniel McClure
 	License: GNU General Public License v3.0
 	License URI: http://www.gnu.org/licenses/gpl-3.0.html
 */
@@ -22,7 +22,7 @@ add_filter( 'job_manager_job_listing_data_fields', 'admin_add_salary_field' );
     // Here we create the function for this custom field in admin  
     function admin_add_salary_field( $fields ) {
   $fields['_job_salary'] = array(
-    'label'       => __( 'Salary ($)', 'job_manager' ),
+    'label'       => __( 'Salary (£)', 'job_manager' ),
     'type'        => 'text',
     'placeholder' => 'e.g. 20000',
     'description' => ''
@@ -36,7 +36,7 @@ add_filter( 'submit_job_form_fields', 'frontend_add_salary_field' );
     // Here we create the function for this custom field in frontend
 function frontend_add_salary_field( $fields ) {
   $fields['job']['job_salary'] = array(
-    'label'       => __( 'Salary ($)', 'job_manager' ),
+    'label'       => __( 'Salary (£)', 'job_manager' ),
     'type'        => 'text',
     'required'    => true,
     'placeholder' => 'e.g. 20000',
@@ -55,6 +55,73 @@ add_action( 'single_job_listing_meta_end', 'display_job_salary_data' );
   $salary = get_post_meta( $post->ID, '_job_salary', true );
 
   if ( $salary ) {
-    echo '<li>' . __( 'Salary:' ) . ' $' . esc_html( $salary ) . '</li>';
+    echo '<li>' . __( 'Salary:' ) . ' £' . esc_html( $salary ) . '</li>';
   }
+}
+
+/**
+ * This can either be done with a filter (below) or the field can be added directly to the job-filters.php template file!
+ *
+ * job-manager-filter class handling was added in v1.23.6
+ */
+add_action( 'job_manager_job_filters_search_jobs_end', 'filter_by_salary_field' );
+
+function filter_by_salary_field() {
+	?>
+	<div class="search_categories">
+		<label for="search_categories"><?php _e( 'Salary', 'wp-job-manager' ); ?></label>
+		<select name="filter_by_salary" class="job-manager-filter">
+			<option value=""><?php _e( 'Any Salary', 'wp-job-manager' ); ?></option>
+			<option value="upto20"><?php _e( 'Up to £20,000', 'wp-job-manager' ); ?></option>
+			<option value="20000-40000"><?php _e( '£20,000 to £40,000', 'wp-job-manager' ); ?></option>
+			<option value="40000-60000"><?php _e( '£40,000 to £60,000', 'wp-job-manager' ); ?></option>
+			<option value="over60"><?php _e( '£60,000+', 'wp-job-manager' ); ?></option>
+		</select>
+	</div>
+	<?php
+}
+
+/**
+ * Adds Salary Search Functionality as per https://wpjobmanager.com/document/tutorial-adding-a-salary-field-for-jobs/
+ */
+add_filter( 'job_manager_get_listings', 'filter_by_salary_field_query_args', 10, 2 );
+function filter_by_salary_field_query_args( $query_args, $args ) {
+	if ( isset( $_POST['form_data'] ) ) {
+		parse_str( $_POST['form_data'], $form_data );
+
+		// If this is set, we are filtering by salary
+		if ( ! empty( $form_data['filter_by_salary'] ) ) {
+			$selected_range = sanitize_text_field( $form_data['filter_by_salary'] );
+			switch ( $selected_range ) {
+				case 'upto20' :
+					$query_args['meta_query'][] = array(
+						'key'     => '_job_salary',
+						'value'   => '20000',
+						'compare' => '<',
+						'type'    => 'NUMERIC'
+					);
+				break;
+				case 'over60' :
+					$query_args['meta_query'][] = array(
+						'key'     => '_job_salary',
+						'value'   => '60000',
+						'compare' => '>=',
+						'type'    => 'NUMERIC'
+					);
+				break;
+				default :
+					$query_args['meta_query'][] = array(
+						'key'     => '_job_salary',
+						'value'   => array_map( 'absint', explode( '-', $selected_range ) ),
+						'compare' => 'BETWEEN',
+						'type'    => 'NUMERIC'
+					);
+				break;
+			}
+
+			// This will show the 'reset' link
+			add_filter( 'job_manager_get_listings_custom_filter', '__return_true' );
+		}
+	}
+	return $query_args;
 }
